59c0a4b6e452606cafbc956dfb46b9d1ccf37937
baileysdk <43979702+baileysdk@users.noreply.github.com>
2022-02-03 12:10:07 -0500

    Final Alterations

--- DropForce/force-app/main/default/aura/SupplierInfoPage/SupplierInfoPageController.js
+++ DropForce/force-app/main/default/aura/SupplierInfoPage/SupplierInfoPageController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-        
+    doInit : function(component, event, helper) {
+        component.set("v.recordId", "0018c000028LutTAAS");
     }
 })
