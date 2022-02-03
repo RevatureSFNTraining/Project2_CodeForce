f6cb02c9ddda853cc81bac2e1afc7b5cba80ee1a
baileysdk <43979702+baileysdk@users.noreply.github.com>
2022-02-03 10:29:55 -0500

    Alterations for Experience Site

--- DropForce/force-app/main/default/lwc/hoverAccountDetailLwc/hoverAccountDetailLwc.js
+++ DropForce/force-app/main/default/lwc/hoverAccountDetailLwc/hoverAccountDetailLwc.js
@@ -1,5 +1,6 @@
-import { api, wire, LightningElement } from 'lwc';
+import { track, api, wire, LightningElement } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import MAIN_MARKET_FIELD from '@salesforce/schema/Account.Main_Market__c';
@@ -7,10 +8,49 @@ import NUMBER_LOCATIONS_FIELD from '@salesforce/schema/Account.NumberofLocations
 import YEAR_STARTED_FIELD from '@salesforce/schema/Account.YearStarted';
 import CERTIFICATIONS_FIELD from '@salesforce/schema/Account.Certifications__c';
 
-export default class HoverObjectDetailLwc extends LightningElement {
+export default class HoverObjectDetailLwc extends NavigationMixin (LightningElement) {
+    @api recordDisplayId;
+    @track CurrentPageReference;
+
+    @wire(CurrentPageReference)
+    setCurrentPageReference(currentPageReference) {
+        this.currentPageReference = currentPageReference;
+    }
+    get recordId() {
+        return this.currentPageReference?.state?.c__recordId;
+    }
+
+    url;
+    url;
+
+    connectedCallback() {
+        // Store the PageReference in a variable to use in handleClick.
+        // This is a plain Javascript object that conforms to the
+        // PageReference type by including 'type' and 'attributes' properties.
+        // The 'state' property is optional.
+        this.accountHomePageRef = {
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Supplier_Info__c'
+            },
+            state: {
+                c__recordId: '01t8c00000HE0JYAA1',
+            }
+        };
+        this[NavigationMixin.GenerateUrl](this.accountHomePageRef)
+            .then(url => this.url = url);
+    }
+
+    handleClick(evt) {
+        // Stop the event's default behavior.
+        // Stop the event from bubbling up in the DOM.
+        evt.preventDefault();
+        evt.stopPropagation();
+        // Navigate to the Account Home page.
+        this[NavigationMixin.Navigate](this.accountHomePageRef);
+    }
 
     // Variable to receive Record Id from Parent Aura Component
-    @api recordDisplayId;
 
     
     // Data Call to receive record information to Display
